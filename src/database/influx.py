56b4126122b4f502b2c5f8305e15bd728f9064f5
FritzJo <fritzjo-git@mailbox.org>
2019-08-22 18:38:49 +0200

    added emanager as datasource

--- src/database/influx.py
+++ src/database/influx.py
@@ -1,21 +1,12 @@
 import datetime
 
 from influxdb import InfluxDBClient
+
 from environment import get_database_host, get_database_port, get_database_user, get_database_pass
 
 
-def add_entry(json_info):
+def add_entry(json_info, emanager_info_json):
     time = datetime.datetime.now()
-
-    # Connect to the database
-    client = InfluxDBClient(get_database_host(),
-                            get_database_port(),
-                            get_database_user(),
-                            get_database_pass(),
-                            'volkswagen')
-
-    client.create_database('volkswagen')
-
     # Create the json containing the relevant data
     json_body = [
         {
@@ -31,8 +22,25 @@ def add_entry(json_info):
             "fields": {
                 "current_range": json_info['vehicleDetails']['range']
             }
+        },
+                {
+            "measurement": "charging",
+            "time": time,
+            "fields": {
+                "chargingState": '1' if emanager_info_json['EManager']['rbc']['status']['chargingState'] == 'ON' else '0',
+                "batteryPercentage": emanager_info_json['EManager']['rbc']['status']['batteryPercentage'],
+            }
         }
     ]
+    
+    # Connect to the database
+    client = InfluxDBClient(get_database_host(),
+                            get_database_port(),
+                            get_database_user(),
+                            get_database_pass(),
+                            'volkswagen')
+
+    client.create_database('volkswagen')
 
     # Write the data to the database
     client.write_points(json_body)
