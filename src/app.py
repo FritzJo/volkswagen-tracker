1ecb1908f7a4f1e886d2262c7badb92fd7728b26
FritzJo <fritzjo-git@mailbox.org>
2019-08-24 13:26:48 +0200

    improved logging

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import logging
 
 from time import sleep
 from database.influx import add_entry
@@ -7,15 +8,19 @@ from database.influx import add_entry
 from vw_carnet import CarNetLogin, CarNetPost
 from environment import get_username, get_password, get_update_interval
 
-#s = requests.Session()  # Create a session to save cookies and connection data
-#url = CarNetLogin(s, get_username(), get_password())  # Login and receive the custom URL for your Carnet account
+logging.basicConfig(format='%(asctime)s %(message)s',level=logging.DEBUG)
 
 """Request data every X hours and write it to the database"""
 time_waited = 0
 while True:
+    logging.info("Refreshing session and credentials")
     s = requests.Session()  
     url = CarNetLogin(s, get_username(), get_password()) 
+    logging.info("Requesting vehicle details from we-connect")
     car_info_json = json.loads(CarNetPost(s, url, '/-/vehicle-info/get-vehicle-details'))
+    logging.info("Requesting e-manager information from we-connect")
     emanager_info_json = json.loads(CarNetPost(s, url, '/-/emanager/get-emanager'))
+    logging.info("Writing data to database")
     add_entry(car_info_json, emanager_info_json)  # Insert the value into the database
+    logging.info("Waiting for " + str(1 * 60 * 60 * int(get_update_interval())) + " hours")
     sleep(1 * 60 * 60 * int(get_update_interval()))  # Wait for X h
