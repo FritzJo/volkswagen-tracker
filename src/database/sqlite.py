0f5515dbfcc1ada250362e7dee7e98f020069fa4
FritzJo <fritzjo-git@mailbox.org>
2019-10-01 11:10:44 +0200

    changed variable name to avoid redefining "range"

--- src/database/sqlite.py
+++ src/database/sqlite.py
@@ -43,11 +43,11 @@ def add_entry(json_info, emanager_info_json):
 
     time = datetime.datetime.now()
     mileage = int(json_info['vehicleDetails']['distanceCovered'].replace(".", ""))
-    range = int(json_info['vehicleDetails']['range'])
+    current_range = int(json_info['vehicleDetails']['range'])
     charge_status = 0 if emanager_info_json['EManager']['rbc']['status']['chargingState'] == 'OFF' else 1
     battery_percentage = emanager_info_json['EManager']['rbc']['status']['batteryPercentage']
 
-    sql = ''' INSERT INTO volkswagen(date,mileage,range,chargeStatus,batteryPercentage)
+    sql = ''' INSERT INTO volkswagen(date,mileage,current_range,chargeStatus,batteryPercentage)
               VALUES(?,?,?,?,?) '''
     cur = conn.cursor()
     cur.execute(sql, time, mileage, range, charge_status, battery_percentage)
