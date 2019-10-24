98f695d39df5dc04e2956bf29e6745e91f9a8290
FritzJo <fritzjo-git@mailbox.org>
2019-10-24 21:02:35 +0200

    fixed type error

--- src/database/local.py
+++ src/database/local.py
@@ -1,3 +1,3 @@
 def add_entry(data):
     with open("database.csv", "a") as f:
-        f.write(data[0] + ";" + data[1] + ";" + data[2] + ";" + data[3] + ";" + data[4])
+        f.write(str(data[0]) + ";" + str(data[1]) + ";" + str(data[2]) + ";" + str(data[3]) + ";" + str(data[4]))
